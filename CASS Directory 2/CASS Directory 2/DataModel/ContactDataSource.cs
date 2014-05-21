fe7e962366b6f85e37c8bf84dc8435f4f6101e95
darshannnn <darshannnn@gmal.com>
2014-05-21 11:18:00 +1000

    BRBDirectory
    Deployed in BRB

---
+++ CASS Directory 2/CASS Directory 2/DataModel/ContactDataSource.cs
@@ -0,0 +1,266 @@
+﻿using CASS_Directory_2.Data;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CASS_Directory_2.DataModel
+{
+    class ContactDataSource
+    {
+        private static ContactDataSource _contactDataSource = new ContactDataSource();
+
+        private ObservableCollection<SampleDataGroup> _allGroups = new ObservableCollection<SampleDataGroup>();
+        public ObservableCollection<SampleDataGroup> AllGroups
+        {
+            get { return this._allGroups; } 
+        }
+
+        public static IEnumerable<SampleDataGroup> GetGroups()
+        {
+            return _contactDataSource.AllGroups;
+        }
+
+        public static IEnumerable<SampleDataGroup> GetFunctionalGroup()
+        {
+            var functionalGroup =  _contactDataSource.AllGroups.SelectMany(group => group.Items).Where(item => item.UserType.Equals("F"));
+            ObservableCollection<SampleDataGroup> searchedResultGroup = new ObservableCollection<SampleDataGroup>();
+
+             var group1 = new SampleDataGroup("functionalGroup",
+                 "Functional Areas",
+                 "Assets/DarkGray.png",
+                 "Group Description: functional Areas");
+
+            searchedResultGroup.Add(group1);
+
+            foreach (SampleDataItem i in functionalGroup)
+                searchedResultGroup.ElementAt(0).Items.Add(i);
+
+            return searchedResultGroup;
+
+        }
+
+        public static SampleDataGroup GetGroup(string uniqueId)
+        {
+            var matches = _contactDataSource.AllGroups.Where((group) => group.UniqueId.Equals(uniqueId));
+            if (matches.Count() == 1) return matches.First();
+            return null;
+        }
+
+        public static SampleDataItem GetItem(string uniqueId)
+        {
+            var matches = _contactDataSource.AllGroups.SelectMany(group => group.Items).Where((item) => item.UniqueId.Equals(uniqueId));
+            if (matches.Count() == 1) return matches.First();
+            return null;
+        }
+
+        public static IEnumerable<SampleDataGroup> SearchGroup(string query)
+        {
+          
+            string [] name = query.Split(new Char [] {' '});
+
+            string first = name[0];
+          //string second = name[1];
+           
+            if(name.Length >1)
+            {
+            
+            var matches = _contactDataSource.AllGroups.SelectMany(group => group.Items).Where((item) => item.GivenName.IndexOf(name[0], StringComparison.OrdinalIgnoreCase) >= 0 && item.Surname.IndexOf(name[1], StringComparison.OrdinalIgnoreCase) >= 0);
+            
+            ObservableCollection<SampleDataGroup> searchedResultGroup = new ObservableCollection<SampleDataGroup>();
+
+            var group1 = new SampleDataGroup("searchedResultGroup-1",
+                 "Search Result",
+                 "Assets/DarkGray.png",
+                 "Group Description: searchedResultGroup");
+
+            searchedResultGroup.Add(group1);
+
+            foreach (SampleDataItem i in matches)
+                searchedResultGroup.ElementAt(0).Items.Add(i);
+
+            return searchedResultGroup;
+            }
+
+            else
+            {
+                var matches = _contactDataSource.AllGroups.SelectMany(group => group.Items).Where((item) => item.GivenName.IndexOf(query, StringComparison.OrdinalIgnoreCase) >= 0 || item.Surname.IndexOf(query, StringComparison.OrdinalIgnoreCase) >= 0);
+            
+            ObservableCollection<SampleDataGroup> searchedResultGroup = new ObservableCollection<SampleDataGroup>();
+
+            var group1 = new SampleDataGroup("searchedResultGroup-1",
+                 "Search Result",
+                 "Assets/DarkGray.png",
+                 "Group Description: searchedResultGroup");
+
+            searchedResultGroup.Add(group1);
+
+            foreach (SampleDataItem i in matches)
+                searchedResultGroup.ElementAt(0).Items.Add(i);
+
+            return searchedResultGroup;
+            }
+        }
+
+        public static IEnumerable<SampleDataGroup> SearchGroupStartsWith(string query)
+        {
+            var firstnameMatches = _contactDataSource.AllGroups.SelectMany(group => group.Items).Where((item) => item.GivenName.StartsWith(query)); //IndexOf(query, StringComparison.OrdinalIgnoreCase) >= 0);
+            var surnameMatches = _contactDataSource.AllGroups.SelectMany(group => group.Items).Where((item) => item.Surname.StartsWith(query));
+
+            ObservableCollection<SampleDataGroup> searchedResultGroup = new ObservableCollection<SampleDataGroup>();
+
+            var firstnameGroup = new SampleDataGroup("searchedResultGroup-1",
+                 "Results By Firstname",
+                 "Assets/DarkGray.png",
+                 "Group Description: searchedResultGroup");
+
+            var surnameGroup = new SampleDataGroup("searchedResultGroup-1",
+                 "Results By Surname",
+                 "Assets/DarkGray.png",
+                 "Group Description: searchedResultGroup");
+
+            searchedResultGroup.Add(firstnameGroup);
+            searchedResultGroup.Add(surnameGroup);
+
+            foreach (SampleDataItem i in firstnameMatches)
+                searchedResultGroup.ElementAt(0).Items.Add(i);
+
+            foreach (SampleDataItem i in surnameMatches)
+                searchedResultGroup.ElementAt(1).Items.Add(i);
+
+            return searchedResultGroup;
+        }
+
+        //public static IEnumerable<SampleDataGroup> SearchGroupUID(string query)
+        //{
+        //    var uidMatches = _contactDataSource.AllGroups.SelectMany(group => group.Items).Where(item => item.UniqueId.StartsWith(query));
+            
+        //    ObservableCollection<SampleDataGroup> searchedResultGroup = new ObservableCollection<SampleDataGroup>();
+
+        //    var uidGroup = new SampleDataGroup("searchedResultGroup-1",
+        //         "Administration Areas",
+        //         "Assets/DarkGray.png",
+        //         "Group Description: searchedResultGroup");
+
+
+        //    searchedResultGroup.Add(uidGroup);
+           
+        //    foreach (SampleDataItem i in uidMatches)
+        //        searchedResultGroup.ElementAt(0).Items.Add(i);
+
+        //    return searchedResultGroup;
+        //}
+
+
+
+
+        public static void refreshData() 
+        {
+            using (MySqlConnection connection = new MySqlConnection("Server=150.203.139.164;Database=rptdb_prod;Uid=remote;Password=CASSAdmin;Connection Timeout=2200"))
+            {
+                connection.Open();
+
+                // Category list
+                MySqlCommand categoriesCommand = new MySqlCommand("SELECT property_cd, property_name FROM space_property WHERE property_cd='H013'", connection);
+                using (MySqlDataReader reader = categoriesCommand.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        SampleDataGroup group = new SampleDataGroup(reader.GetString("property_cd"),
+                          reader.GetString("property_name"),
+                          "Assets/LightGray.png",
+                          string.Empty);
+                    
+
+                        // Film list by Category
+                        using (MySqlConnection filmsConnection = (MySqlConnection)connection.Clone())
+                        {
+                            filmsConnection.Open();
+                            MySqlCommand filmsCommand = new MySqlCommand(@"SELECT ifnull(uni_id,'') as uni_id,ifnull(given_name,'') as given_name,ifnull(surname,'') as surname,ifnull(user_type,'') as user_type,ifnull(tel_no,'') as tel_no,ifnull(email,'') as email,
+                                                                           ifnull(level_no,'') as level_no,ifnull(room_no,'') as room_no FROM space_room_allocation"
+                                                                           + " WHERE given_name <> '' AND property_cd = ?id ORDER BY given_name",
+                                                                            filmsConnection);
+                            filmsCommand.Parameters.Add(new MySqlParameter("?id", reader.GetString("property_cd")));
+                            using (MySqlDataReader filmsReader = filmsCommand.ExecuteReader())
+                            {
+                                while (filmsReader.Read())
+                                {
+                                    group.Items.Add(new SampleDataItem(filmsReader.GetString("uni_id"),
+                                    filmsReader.GetString("given_name"),
+                                    filmsReader.GetString("surname"),
+                                     filmsReader.GetString("user_type"),
+                                    filmsReader.GetString("tel_no"),
+                                    filmsReader.GetString("email"),
+                                    filmsReader.GetString("level_no") + "." + filmsReader.GetString("room_no"),
+                                    "Assets/DarkGray.png",
+                                    string.Empty,
+                                    string.Empty,
+                                    group));
+
+
+                                }
+                            }
+                        }
+                        _contactDataSource.AllGroups.Clear();
+                        _contactDataSource.AllGroups.Add(group);
+                    }
+                }
+            }
+        }
+
+
+        public ContactDataSource()
+        {
+            using (MySqlConnection connection = new MySqlConnection("Server=150.203.139.164;Database=rptdb_prod;Uid=remote;Password=CASSAdmin;Connection Timeout=2200"))
+            {
+                connection.Open();
+
+                // Category list
+                MySqlCommand categoriesCommand = new MySqlCommand("SELECT property_cd, property_name FROM space_property WHERE property_cd='H013'", connection);
+                using (MySqlDataReader reader = categoriesCommand.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        SampleDataGroup group = new SampleDataGroup(reader.GetString("property_cd"),
+                          reader.GetString("property_name"),
+                          "Assets/LightGray.png",
+                          string.Empty);
+
+                        // Film list by Category
+                        using (MySqlConnection filmsConnection = (MySqlConnection)connection.Clone())
+                        {
+                            filmsConnection.Open();
+                            MySqlCommand filmsCommand = new MySqlCommand(@"SELECT ifnull(uni_id,'') as uni_id,ifnull(given_name,'') as given_name,ifnull(surname,'') as surname,ifnull(user_type,'') as user_type,ifnull(tel_no,'') as tel_no,ifnull(email,'') as email,
+                                                                           ifnull(level_no,'') as level_no,ifnull(room_no,'') as room_no FROM space_room_allocation",
+                                                                            filmsConnection);
+                            filmsCommand.Parameters.Add(new MySqlParameter("?id", reader.GetString("property_cd")));
+                            using (MySqlDataReader filmsReader = filmsCommand.ExecuteReader())
+                            {
+                                while (filmsReader.Read())
+                                {
+                                    group.Items.Add(new SampleDataItem(filmsReader.GetString("uni_id"),
+                                    filmsReader.GetString("given_name"),
+                                    filmsReader.GetString("surname"),
+                                     filmsReader.GetString("user_type"),
+                                    filmsReader.GetString("tel_no"),
+                                    filmsReader.GetString("email"),
+                                    filmsReader.GetString("level_no") + "." + filmsReader.GetString("room_no"),
+                                    "Assets/DarkGray.png",
+                                    string.Empty,
+                                    string.Empty,
+                                    group));
+
+
+                                }
+                            }
+                        }
+                        this.AllGroups.Add(group);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ CASS Directory 2/CASS Directory 2/GroupedItemsPage.xaml.cs
@@ -0,0 +1,154 @@
+﻿using CASS_Directory_2.Data;
+using CASS_Directory_2.DataModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Grouped Items Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234231
+
+namespace CASS_Directory_2
+{
+    /// <summary>
+    /// A page that displays a grouped collection of items.
+    /// </summary>
+    public sealed partial class GroupedItemsPage : CASS_Directory_2.Common.LayoutAwarePage
+    {
+        DispatcherTimer dTimer = new DispatcherTimer();
+
+        public GroupedItemsPage()
+        {
+            this.InitializeComponent();
+             
+        }
+
+        /// <summary>
+        /// Populates the page with content passed during navigation.  Any saved state is also
+        /// provided when recreating a page from a prior session.
+        /// </summary>
+        /// <param name="navigationParameter">The parameter value passed to
+        /// <see cref="Frame.Navigate(Type, Object)"/> when this page was initially requested.
+        /// </param>
+        /// <param name="pageState">A dictionary of state preserved by this page during an earlier
+        /// session.  This will be null the first time a page is visited.</param>
+        protected override void LoadState(Object navigationParameter, Dictionary<String, Object> pageState)
+        {
+            // TODO: Create an appropriate data model for your problem domain to replace the sample data
+           // var sampleDataGroups = SampleDataSource.GetGroups((String)navigationParameter);
+         //   var contactDataGroups = ContactDataSource.GetGroups();
+            var contactDataGroups = ContactDataSource.GetFunctionalGroup();
+            this.DefaultViewModel["Groups"] = contactDataGroups;
+            dTimer.Tick += dTimer_Tick;
+            int hrs = 0;
+            int mins = 0;
+            int sec = 60;
+            dTimer.Interval = new TimeSpan(hrs, mins, sec);
+        }
+
+        void dTimer_Tick(object sender, object e)
+        {
+            Home(this, new RoutedEventArgs());
+        }
+
+        /// <summary>
+        /// Invoked when a group header is clicked.
+        /// </summary>
+        /// <param name="sender">The Button used as a group header for the selected group.</param>
+        /// <param name="e">Event data that describes how the click was initiated.</param>
+        void Header_Click(object sender, RoutedEventArgs e)
+        {
+            // Determine what group the Button instance represents
+            var group = (sender as FrameworkElement).DataContext;
+
+            // Navigate to the appropriate destination page, configuring the new page
+            // by passing required information as a navigation parameter
+            this.Frame.Navigate(typeof(GroupDetailPage), ((SampleDataGroup)group).UniqueId);
+        }
+
+        /// <summary>
+        /// Invoked when an item within a group is clicked.
+        /// </summary>
+        /// <param name="sender">The GridView (or ListView when the application is snapped)
+        /// displaying the item clicked.</param>
+        /// <param name="e">Event data that describes the item clicked.</param>
+        void ItemView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            // Navigate to the appropriate destination page, configuring the new page
+            // by passing required information as a navigation parameter
+            var itemId = ((SampleDataItem)e.ClickedItem).UniqueId;
+            this.Frame.Navigate(typeof(ItemDetailPage), itemId);
+        }
+
+
+        private void SearchBoxEventsSuggestionsRequested(SearchBox sender, SearchBoxSuggestionsRequestedEventArgs e)
+        {
+            var contactDataGroups = ContactDataSource.GetGroups();
+            string queryText = e.QueryText;
+            if (!string.IsNullOrEmpty(queryText))
+            {
+                Windows.ApplicationModel.Search.SearchSuggestionCollection suggestionCollection = e.Request.SearchSuggestionCollection;
+                foreach (SampleDataGroup group in contactDataGroups)
+                {
+                    foreach (SampleDataItem item in group.Items)
+                    {
+
+                        if (item.GivenName.StartsWith(queryText, StringComparison.CurrentCultureIgnoreCase) || item.Surname.StartsWith(queryText, StringComparison.CurrentCultureIgnoreCase))
+                            suggestionCollection.AppendQuerySuggestion(item.GivenName + " " + item.Surname);
+                    }
+
+                }
+
+            }
+        }
+
+        private void SearchBoxEventsQuerySubmitted(object sender, SearchBoxQuerySubmittedEventArgs e)
+        {
+            //var itemId = ((SampleDataItem)e.ClickedItem).UniqueId;
+            //this.Frame.Navigate(typeof(ItemDetailPage), itemId);
+            ////MainPage.Current.NotifyUser(e.QueryText, NotifyType.StatusMessage);
+            //itemGridView.SetBinding = 
+            
+            var searchResult = ContactDataSource.SearchGroup(e.QueryText);
+            this.DefaultViewModel["Groups"] = searchResult;
+            SearchContacts.QueryText = "";
+            dTimer.Start();
+            //Binding searchedBinding = new Binding();
+            //searchedBinding.Source = "searchResult";
+
+            //itemGridView.SetBinding(GridView.DataContextProperty,searchedBinding);
+
+            
+
+        }
+
+        private void Alpha_Click(object sender, RoutedEventArgs e)
+        {
+            Button al = (Button)sender;
+            var searchResult = ContactDataSource.SearchGroupStartsWith(al.Name);
+            this.DefaultViewModel["Groups"] = searchResult;
+            dTimer.Start();
+        }
+
+        private void Home(object sender, RoutedEventArgs e)
+        {
+            var contactDataGroups = ContactDataSource.GetFunctionalGroup();
+            this.DefaultViewModel["Groups"] = contactDataGroups;
+            dTimer.Stop();
+            ContactDataSource.refreshData();
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            SearchContacts.QueryText = "";
+        }
+    }
+}
